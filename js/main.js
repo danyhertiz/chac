d62b93c82c6188c627378ca221fe89dbba7e1842
Genkidan1984 <danyhertiz@outlook.com>
2025-08-15 20:18:00 -0600

    Menú deslizable y nuevo switch

--- js/main.js
+++ js/main.js
@@ -1,31 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Sitio web cargado y listo.');
-
-    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const switchBtn = document.getElementById('modeSwitch');
 
     const enableDarkMode = () => {
-        document.body.classList.add('dark-mode');
+        document.documentElement.classList.add('dark');
         localStorage.setItem('darkMode', 'enabled');
-        darkModeToggle.textContent = '☀️';
     };
 
     const disableDarkMode = () => {
-        document.body.classList.remove('dark-mode');
+        document.documentElement.classList.remove('dark');
         localStorage.setItem('darkMode', 'disabled');
-        darkModeToggle.textContent = '🌙';
     };
 
-    if (localStorage.getItem('darkMode') === 'enabled') {
-        enableDarkMode();
-    }
-
-    darkModeToggle.addEventListener('click', () => {
+    switchBtn.addEventListener('click', () => {
         if (localStorage.getItem('darkMode') !== 'enabled') {
             enableDarkMode();
         } else {
             disableDarkMode();
         }
     });
-
-    // Futuras funciones interactivas irán aquí.
 });
